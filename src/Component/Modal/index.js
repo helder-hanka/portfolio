adf8dfe2513648c93555c8cff42f7226e6e044cc
helder-hanka <hfernandes238@gmail.com>
2024-10-07 06:09:35 +0200

    Add Modal bootstrap

--- src/Component/Modal/index.js
+++ src/Component/Modal/index.js
@@ -1,16 +1,86 @@
-import React from 'react'
-import './Modal.css'
+import { Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { GiClick } from "react-icons/gi";
+import "./Modal.css";
 
-const Modal = ({showModal,closeModal, children}) => {
-    return (
-        showModal && (
-            <div className="modalBackground" onClick={closeModal}>
-                <div className="modalContainer">
-                    {children}
-                </div>
-            </div>
-        )
-    )
-}
+const NewsModal = ({ show, onHide, showModalInfo }) => {
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {showModalInfo?.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {Array.isArray(showModalInfo.image) ? (
+          <img
+            src={showModalInfo?.image[0]}
+            alt={showModalInfo.title}
+            className="modal-img"
+          />
+        ) : (
+          <img src={showModalInfo?.image} alt={showModalInfo.title} />
+        )}
+        <section className="textsContainer">
+          <div className="textInfo">
+            {showModalInfo?.texts?.map((i, index) => (
+              <p key={index}>{i}</p>
+            ))}
+          </div>
 
-export default Modal
+          {showModalInfo?.url && (
+            <p>
+              <a
+                href={showModalInfo.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                voir le site
+              </a>
+              <GiClick /> <br />
+            </p>
+          )}
+          {showModalInfo?.sourceFront && (
+            <p>
+              <a
+                href={showModalInfo.sourceFront}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github: Front-end
+              </a>
+              <GiClick /> <br />
+            </p>
+          )}
+          {showModalInfo?.sourceBack && (
+            <p>
+              <a
+                href={showModalInfo.sourceBack}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github: Back-end
+              </a>
+              <GiClick /> <br />
+            </p>
+          )}
+        </section>
+      </Modal.Body>
+      <Modal.Footer>
+        {showModalInfo?.language?.map((i, index) => (
+          <p className="langages" key={index}>
+            <strong>{i} </strong>
+          </p>
+        ))}
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default NewsModal;
